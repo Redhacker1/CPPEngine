8e933708b5620064c908b9c759e1286c96d2896f
Redhacker1 <66490743+Redhacker1@users.noreply.github.com>
2023-01-03 14:15:17 -0600

    remove BGFX for the moment, will look into using raw DX11 and DXVK-Native

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
-#include <SDL2/SDL.h>
+#include "SDL.h"
 #include <random>
 #include <iostream>
-#include "modules/thirdparty/bgfx_cmake/bgfx/include/bgfx/bgfx.h"
 #include <chrono>
+#include <SDL_syswm.h>
 
 
 int main(int argc, char* args[])
 {
 
     SDL_Init(SDL_INIT_VIDEO);
-
-    bgfx::Init();
-
     SDL_Window *window = SDL_CreateWindow(
             "SDL2Demo",
             SDL_WINDOWPOS_UNDEFINED,
@@ -21,10 +18,12 @@ int main(int argc, char* args[])
             0
     );
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_SOFTWARE);
+    SDL_SysWMinfo wmInfo;
+    SDL_VERSION(&wmInfo.version);
+    SDL_GetWindowWMInfo(window, &wmInfo);
+    HWND hwnd = wmInfo.info.win.window;
 
     SDL_Event input;
-    int r = 0, g = 255, b = 0;
     bool quit = false;
 
     while (!quit)
@@ -36,42 +35,26 @@ int main(int argc, char* args[])
                 quit = true;
             }
             else if (input.type == SDL_KEYDOWN) {
-                if (input.key.repeat == 0) {
-                    std::cout << SDL_GetScancodeName(input.key.keysym.scancode) << std::endl;
-                }
-                //...randomize the renderer's draw color
-                r = std::rand() % 255;
-                g = std::rand() % 255;
-                b = std::rand() % 255;
+
             }
             else if (input.type == SDL_KEYUP)
             {
                 if(input.key.repeat == 0)
                 {
-                    std::cout << SDL_GetScancodeName(input.key.keysym.scancode) << " is up"<< std::endl;
+
                 }
             }
 
-            SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-
-            SDL_DisplayMode displayMode;
-            SDL_GetWindowDisplayMode(window, &displayMode);
-
-            SDL_Rect Rect = SDL_Rect();
-            Rect.w = displayMode.w;
-            Rect.h = displayMode.h;
-
-            SDL_RenderDrawRect(renderer,&Rect);
 
         }
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+
     }
 
 
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();;
 
     return 0;
 }
+
+
